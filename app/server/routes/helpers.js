6493b02250c46ff6ea399e12b4e20b8adea7110b
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2014-02-19 13:22:19 +0100

    some fix on bcrypt

--- app/server/routes/helpers.js
+++ app/server/routes/helpers.js
@@ -1,4 +1,4 @@
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 var ObjectID = require('mongodb').ObjectID;
 var accounting = require('accounting');
 accounting.settings = _config.accountingSettings;
