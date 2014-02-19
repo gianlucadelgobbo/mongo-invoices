e7236ce3027498b6d61d4995190fa957d6a68b8a
gianlucadelgobbo <g.delgobbo@flyer.it>
2014-02-19 12:20:36 +0000

    config

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ global.host = 'localhost';
 require('./app/setup')(app, exp);
 require('./app/server/router')(app);
 
-<<<<<<< HEAD
-app.listen(8000, function(){
-=======
-app.listen(8003, function(){
->>>>>>> 76c7f54683f84df84f7b9728c952da133448c7f9
+app.listen(8001, function(){
 	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
